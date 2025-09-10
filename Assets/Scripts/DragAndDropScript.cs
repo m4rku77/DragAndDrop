fbb31edb14fa25a7b52e37b76406c88bb90ae6f1
Bugsters104 <vards.uzvards@pasts.lv>
2025-09-10 11:38:51 +0300

    50% of DragAndDrop

--- Assets/Scripts/DragAndDropScript.cs
+++ Assets/Scripts/DragAndDropScript.cs
@@ -9,6 +9,7 @@ public class DragAndDropScript : MonoBehaviour, IPointerDownHandler, IBeginDragH
     private CanvasGroup canvasGro;
     private RectTransform rectTra;
     public ObjectScript objectScr;
+    public ScreenBoundriesScript screenBou;
 
     // Start is called before the first frame update
     void Start()
@@ -28,12 +29,34 @@ public class DragAndDropScript : MonoBehaviour, IPointerDownHandler, IBeginDragH
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-       //........
+       if(Input.GetMouseButton(0) && !Input.GetMouseButton(1) && !Input.GetMouseButton(2))
+        {
+           objectScr.lastDragged = null;
+            canvasGro.blocksRaycasts = false;
+            canvasGro.alpha = 0.6f;
+            rectTra.SetAsLastSibling();
+           Vector3 cursorWorldPos = Camera.main.ScreenToWorldPoint(
+               new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenBou.screenPoint.z));
+            rectTra.position = cursorWorldPos;
+
+            screenBou.screenPoint = Camera.main.WorldToScreenPoint(rectTra.localPosition);
+
+            screenBou.offset = rectTra.localPosition - 
+                Camera.main.ScreenToWorldPoint(
+                    new Vector3(Input.mousePosition.x, Input.mousePosition.y, 
+                screenBou.screenPoint.z));
+        }
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        //........
+        if (Input.GetMouseButton(0) && !Input.GetMouseButton(1) && !Input.GetMouseButton(2))
+        {
+            Vector3 curSreenPoint = 
+                new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenBou.screenPoint.z);
+            Vector3 curPosition = Camera.main.ScreenToWorldPoint(curSreenPoint) + screenBou.offset;
+            rectTra.position = screenBou.GetClampedPosition(curPosition);
+        }
     }
 
     public void OnEndDrag(PointerEventData eventData)
--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # DragAndDropGames
 Unity 2D Drag And Drop games for children +6
 
-** To do list:
-- [ ] Create the necessary folders 
-- [ ] Add necessary assets 
-- [ ] Add cars on the map
+**To do list:
+- [x] Create the necessary folders 
+- [x] Add necessary assets 
+- [x] Add cars on the map
 - [ ] Create C# script for drag and drop
 - [ ] Create C# script for transformation
 - [ ] Create C# script for object fixation
