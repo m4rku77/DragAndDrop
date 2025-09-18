2f8da607dfb49e384c728d39b53c68427d5e033c
Bugsters104 <vards.uzvards@pasts.lv>
2025-09-18 14:18:40 +0300

    Almoast done

---
+++ Assets/Scripts/FlyingObjectSpawnScript.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class FlyingObjectSpawnScript : MonoBehaviour
+{
+    ScreenBoundriesScript screenBoundriesScript;
+    public GameObject[] cludsPrefabs;
+    public GameObject[] objectPrefabs;
+    public Transform spawnPoint;
+
+    public float cloudSpawnInterval = 2f;
+    public float objectSpawnInterval = 3f;
+    private float minY, maxY;
+    public float cloudMinSpeed = 1.5f;
+    public float cloudMaxSpeed = 150f;
+    public float objectMinSpeed = 2f;
+    public float objectMaxSpeed = 200f;
+
+
+
+    void Start()
+    {
+        screenBoundriesScript = FindFirstObjectByType<ScreenBoundriesScript>();
+        minY = screenBoundriesScript.minY;
+        maxY = screenBoundriesScript.maxY;
+        InvokeRepeating(nameof(SpawnCloud), 0f, cloudSpawnInterval);
+        InvokeRepeating(nameof(SpawnObject), 0f, objectSpawnInterval);
+    }
+
+    void SpawnCloud()
+    {
+        if(cludsPrefabs.Length == 0) 
+            return;
+
+        GameObject cloudPrefab = cludsPrefabs[Random.Range(0, cludsPrefabs.Length)];
+        float y = Random.Range(minY, maxY);
+        Vector3 spawnPosition = new Vector3(spawnPoint.position.x, y, spawnPoint.position.z);
+        GameObject cloud = 
+            Instantiate(cloudPrefab, spawnPosition, Quaternion.identity, spawnPoint);
+        float movementSpeed = Random.Range(cloudMinSpeed, cloudMaxSpeed);   
+        FlyingObjectsControllerScript controller = 
+            cloud.GetComponent<FlyingObjectsControllerScript>();
+        controller.speed = movementSpeed;
+
+    }
+
+    void SpawnObject()
+    {
+
+    }
+}
